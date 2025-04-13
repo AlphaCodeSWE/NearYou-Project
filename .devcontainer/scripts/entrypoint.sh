1cf08957d9d9e8d9c6486ac25af7a451647f0335
Nicoló <nicolobovo@icloud.com>
2025-04-13 17:17:19 +0200

    agg

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Inizio configurazione: imposto ownership e permessi su /opt/airflow_home..."
-if chown -R airflow /opt/airflow_home; then
-    echo "Ownership impostato correttamente."
-else
-    echo "Errore nell'impostazione dell'ownership." >&2
-    exit 1
-fi
+echo "Inizio configurazione: imposto ownership e permessi su /opt/airflow_home (escludendo 'dags')..."
 
-if chmod -R 777 /opt/airflow_home; then
-    echo "Permessi impostati correttamente."
-else
-    echo "Errore nell'impostazione dei permessi." >&2
-    exit 1
-fi
+# Modifica ownership e permessi su tutte le directory/ file in /opt/airflow_home tranne la cartella 'dags'
+find /opt/airflow_home -mindepth 1 -maxdepth 1 ! -name 'dags' -exec chown -R airflow {} +
+find /opt/airflow_home -mindepth 1 -maxdepth 1 ! -name 'dags' -exec chmod -R 777 {} +
+
+echo "Ownership e permessi impostati correttamente (esclusa la cartella 'dags')."
 
 echo "Avvio di Airflow Scheduler come utente 'airflow'..."
 exec su airflow -c "airflow scheduler"
