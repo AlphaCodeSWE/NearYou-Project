0c88d3ce521bc5e36265d646ae6c50c7095a5f73
Nicoló <nicolobovo@icloud.com>
2025-04-13 21:08:28 +0200

    agg

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Controllo inizializzazione del database..."
-# Eseguiamo i comandi airflow come utente airflow
-if ! su airflow -c "airflow db check" > /dev/null 2>&1; then
-    echo "Database non inizializzato: eseguo 'airflow db init' e 'airflow db upgrade'..."
-    su airflow -c "airflow db init"
-    su airflow -c "airflow db upgrade"
-else
-    echo "Il database è già inizializzato."
-fi
-
 echo "Inizio configurazione: imposto ownership e permessi su /opt/airflow_home..."
 if chown -R airflow /opt/airflow_home; then
     echo "Ownership impostato correttamente."
@@ -24,6 +14,15 @@ if chmod -R 777 /opt/airflow_home; then
 else
     echo "Errore nell'impostazione dei permessi." >&2
     exit 1
+fi
+
+echo "Controllo inizializzazione del database..."
+if ! su airflow -c "airflow db check" > /dev/null 2>&1; then
+    echo "Il database non è inizializzato: eseguo 'airflow db init' e 'airflow db upgrade'..."
+    su airflow -c "airflow db init"
+    su airflow -c "airflow db upgrade"
+else
+    echo "Il database è già inizializzato."
 fi
 
 echo "Avvio di Airflow Scheduler come utente 'airflow'..."
