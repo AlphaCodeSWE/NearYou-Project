2b7f7df3d39cb901c0992d7ddc25d739e21d74e4
Nicoló <nicolobovo@icloud.com>
2025-04-13 16:12:32 +0200

    AGG

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+set -e
+
 echo "Inizio configurazione: imposto ownership e permessi su /opt/airflow_home..."
-chown -R airflow:airflow /opt/airflow_home
-chmod -R 777 /opt/airflow_home
-echo "Permessi configurati. Avvio di Airflow Scheduler come utente 'airflow'..."
-exec su airflow -c 'exec airflow scheduler'
+if chown -R airflow:airflow /opt/airflow_home; then
+    echo "Ownership impostato correttamente."
+else
+    echo "Errore nell'impostazione dell'ownership." >&2
+    exit 1
+fi
+
+if chmod -R 777 /opt/airflow_home; then
+    echo "Permessi impostati correttamente."
+else
+    echo "Errore nell'impostazione dei permessi." >&2
+    exit 1
+fi
+
+echo "Avvio di Airflow Scheduler come utente 'airflow'..."
+exec su airflow -c "airflow scheduler"
