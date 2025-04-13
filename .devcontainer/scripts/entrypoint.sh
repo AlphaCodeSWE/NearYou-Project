d0b3b753cecf64b4ba99fb2c89fe08dab21143b6
Nicoló <nicolobovo@icloud.com>
2025-04-13 11:24:22 +0200

    agg

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
+
 if [ ! -f "$AIRFLOW_HOME/airflow.cfg" ]; then
   echo "airflow.cfg non trovato. Inizializzazione del database Airflow..."
-  python -m airflow db init
+  airflow db init
 fi
-exec python -m airflow "$@"
+
+exec airflow "$@"
