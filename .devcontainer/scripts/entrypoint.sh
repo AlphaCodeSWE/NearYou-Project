3e3bcb76ef886319c2a1126d25824cb8ad3a17f1
Nicoló <nicolobovo@icloud.com>
2025-04-13 15:41:41 +0200

    agg

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "Imposto ownership e permessi su /opt/airflow_home..."
+echo "Inizio configurazione: imposto ownership e permessi su /opt/airflow_home..."
 chown -R airflow:airflow /opt/airflow_home
 chmod -R 777 /opt/airflow_home
-echo "Avvio di Airflow Scheduler come utente airflow..."
+echo "Permessi configurati. Avvio di Airflow Scheduler come utente 'airflow'..."
 exec su airflow -c 'exec airflow scheduler'
