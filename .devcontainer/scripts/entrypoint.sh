db60921b8dfe742819e7503cbc210ad28eb33bcd
Nicoló <nicolobovo@icloud.com>
2025-04-13 11:15:51 +0200

    AGG

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
-
-# Se il file airflow.cfg non esiste, inizializza il database Airflow
 if [ ! -f "$AIRFLOW_HOME/airflow.cfg" ]; then
   echo "airflow.cfg non trovato. Inizializzazione del database Airflow..."
-  airflow db init
+  python -m airflow db init
 fi
-
-exec airflow "$@"
+exec python -m airflow "$@"
