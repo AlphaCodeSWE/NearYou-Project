c31ec1a1bbe4cdd8d660443fc4c4cbab98776c4d
Nicoló <nicolobovo@icloud.com>
2025-05-06 20:49:08 +0200

    fix

--- appsota/import.sh
+++ appsota/import.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) Database migration e default roles
+# 1) Migration e default roles
 superset db upgrade
 export FLASK_APP=superset
 
-# 2) Crea admin user (admin/admin)
+# 2) Crea admin user
 superset fab create-admin \
   --username admin \
   --firstname Admin \
@@ -17,20 +17,21 @@ superset fab create-admin \
 superset init
 
 # 4) Aggiungi i datasources
+# URIs con password url‑encoded: @ → %40
 superset datasource add \
   --name UsersEvents \
-  --database-uri clickhouse://default:pwe@123@l@@clickhouse:8123/nearyou \
+  --database-uri "clickhouse://default:pwe%40123%40l%40@clickhouse:8123/nearyou" \
   --table-name user_events
 
 superset datasource add \
   --name Shops \
-  --database-uri postgresql+psycopg2://nearuser:nearypass@postgres-postgis:5432/near_you_shops \
+  --database-uri "postgresql+psycopg2://nearuser:nearypass@postgres-postgis:5432/near_you_shops" \
   --table-name shops
 
-# 5) Importa dashboard + charts definita in JSON
-superset import-dashboards -p /app/definitions/admin_overview.json
+# 5) Importa dashboard + charts
+/app/import_dashboards_and_charts.sh
 
-# 6) Avvia superset
+# 6) Avvia Superset
 exec gunicorn \
   --bind 0.0.0.0:8088 \
   --workers 3 \
